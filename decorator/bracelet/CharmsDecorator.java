ff21d71d6b15e9424c9b2fcdb4dbcc1373c190e7
epickard <epickard@users.noreply.github.com>
2015-07-01 16:02:19 -0700

    Added more complete comments

--- decorator/bracelet/CharmsDecorator.java
+++ decorator/bracelet/CharmsDecorator.java
@@ -3,9 +3,11 @@ package design_patterns_study.decorator.bracelet;
 //Abstract decorator class
 public abstract class CharmsDecorator extends Bracelet {
 	
+	@Override
 	public abstract String getBraceletName();
 	
 	//Each concrete decorator will need to add behavior to cost()
+	@Override
 	public abstract double cost();
 	
 }
