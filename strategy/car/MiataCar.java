bd7606ef2915ff3bbff75768e42a86bc1d41adf3
epickard <epickard@users.noreply.github.com>
2015-08-06 12:35:33 -0700

    Updating name text so that it makes more sense with the text printed in the test class

--- strategy/car/MiataCar.java
+++ strategy/car/MiataCar.java
@@ -4,7 +4,7 @@ public class MiataCar extends Car {
 	
 	//Constructor defines what kind of concrete behavior is assigned to each of the superclass's instance variables
 	public MiataCar () {
-		name = "Name of this car is MIATA";
+		name = "MIATA";
 		startBehavior = new StartWithButton();
 		honkBehavior = new HonkWithCarHorn();
 	}
--- strategy/car/ModelTCar.java
+++ strategy/car/ModelTCar.java
@@ -4,7 +4,7 @@ public class ModelTCar extends Car {
 
 	//Constructor defines what kind of concrete behavior is assigned to each of the superclass's instance variables
 	public ModelTCar() {
-		name = "The name of this car is MODELT";
+		name = "MODEL T";
 		startBehavior = new StartWithCrank();
 		honkBehavior = new HonkWithOldTimeyHorn();
 	}
