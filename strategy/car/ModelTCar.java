6b11de1adc8f7be6875b8411907cfdb916021e23
epickard <epickard@users.noreply.github.com>
2015-08-05 22:29:59 -0700

    Change to use the StartWithCrank implementation of StartBehavior

--- strategy/car/ModelTCar.java
+++ strategy/car/ModelTCar.java
@@ -5,7 +5,7 @@ public class ModelTCar extends Car {
 	//Constructor defines what kind of concrete behavior is assigned to each of the superclass's instance variables
 	public ModelTCar() {
 		name = "The name of this car is MODELT";
-		startBehavior = new StartWithKey();
+		startBehavior = new StartWithCrank();
 		honkBehavior = new HonkWithOldTimeyHorn();
 	}
 }
